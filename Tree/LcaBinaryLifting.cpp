87aa197d40b2c4241bd9bed67169f14da8a6353a
Krishnaneel <107858011+Krishnaneel@users.noreply.github.com>
2023-04-23 18:41:25 +0530

    Update LcaBinaryLifting.cpp

--- Tree/LcaBinaryLifting.cpp
+++ Tree/LcaBinaryLifting.cpp
@@ -34,18 +34,19 @@ int binaryLifting(int node, int k) {
 }
  
 int lca(int a, int b) {
-    if (a == b) return a;
-    if (depth[a] > depth[b]) swap(a, b);
-    int diff = depth[b] - depth[a];
-    b = binaryLifting(b, diff);
-    if (a == b) return a;
-    for (int j = x; j >= 0; j--) {
+    if (a == b) return a;   //if both the node are same
+    if (depth[a] > depth[b]) swap(a, b);   //assuming depth[b]>depth[a] or else swaping the value
+    int diff = depth[b] - depth[a];    //difference between the depth of two nodes
+    b = binaryLifting(b, diff);      //moving node b to same level as a, by binaryLifting 
+    if (a == b) return a;           // if both the nodes are same
+ 
+    for (int j = x; j >= 0; j--) {      //making the maximum jump till the parents of both the nodes are not same)
         if (dp[a][j] != dp[b][j]) {
             a = dp[a][j];
             b = dp[b][j];
         }
     }
-    return dp[a][0];
+    return dp[a][0];   //since the parents of two nodes are dissimilar, we know that the next node has to be the LCA,therefore jumping only 1step
 }
  
 int main() {
@@ -59,12 +60,12 @@ int main() {
         adj[a].push_back(b);
         adj[b].push_back(a);
     }
-    depth[1] = 0;
-    dfs(1, 0, adj);
+    depth[1] = 0;   //depth of root is 0;
+    dfs(1, 0, adj);   //has to take parent of root as 0 not -1;
     while (q--) {
         int m, n;
         cin >> m >> n;
-        int ans=depth[m]+depth[n]-(2*depth[lca(m,n)]);
+        int ans=depth[m]+depth[n]-(2*depth[lca(m,n)]);    // formula !!! derieve it :))
         cout<<ans<<"\n";
     }
     return 0;
